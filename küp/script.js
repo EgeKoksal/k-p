35b8afe42f8071b437c57cae6bff136cec8caa80
Ege Köksal <132518271+EgeKoksal@users.noreply.github.com>
2024-07-25 21:24:54 +0300

    Add files via upload

---
+++ küp/script.js
@@ -0,0 +1,25 @@
+const tiklama = document.querySelectorAll('.tiklama');
+
+// tiklama.forEach (tiklama => {
+// tiklama.addEventListener("click", () => {
+// tiklama.style.transform = "rotateZ(360deg) rotateY(360deg)"
+// })
+// })
+
+
+// tiklama.forEach (tiklama => {
+// tiklama.addEventListener("click", () => {
+// tiklama.style.transform = "rotateZ(360deg) rotateY(360deg)"
+// })
+// })
+
+
+// tiklama.forEach (tiklama => {
+// tiklama.addEventListener("click", () => {
+//   tiklama.classList.add('expanded');
+//      tiklama.textContent = '';
+//      const input = document.createElement('textarea');
+//       input.placeholder = 'Buraya yazın...';
+//        tiklama.appendChild(input);
+//  })
+//  });
